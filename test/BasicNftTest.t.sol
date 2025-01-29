fe7f8cb66f456fdc51af80cbd83b2705b0d15641
Toby <0xdegn@gmail.com>
2025-01-29 19:50:55 +0100

    first commit

---
+++ test/BasicNftTest.t.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+import {Test, console} from "forge-std/Test.sol";
+import {BasicNft} from "../src/BasicNft.sol";
+import {DeployBasicNft} from "../script/DeployBasicNft.s.sol";
+
+contract BasicNftTest is Test {
+    BasicNft public basicNft;
+    DeployBasicNft public deployer;
+
+    function setUp() public {
+        deployer = new DeployBasicNft();
+        basicNft = deployer.run();
+    }
+
+    function testNameIsCorrect() public view {
+        string memory expectedName = "goldie";
+        string memory actualName = basicNft.name();
+
+        assert(keccak256(abi.encodePacked(expectedName)) == keccak256(abi.encodePacked(actualName)));
+    }
+}
